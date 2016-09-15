2abde61a417ecd0e0d9c58b5331999cbc9dbb7fe
DESKTOP-DA0EMKE\artur <artursmolin@gmail.com>
2016-09-15 17:37:57 +0300

    new buttons, icons, more adaptive 360px width

--- materialize/js/js.js
+++ materialize/js/js.js
@@ -1,1 +1,18 @@
 $(".dropdown-button").dropdown()
+$('.button-collapse').sideNav({
+      menuWidth: 200, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+
+// Show sideNav
+  // Hide sideNav
+  $('.button-collapse').sideNav('hide');
+
+
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+          
